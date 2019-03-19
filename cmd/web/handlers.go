28a1edcaf2a46d7ceac22e97a042d92278d04387
Willi Meierhof <Willi.Meierhof@tracetronic.de>
2019-03-19 21:24:03 +0100

    added git ignore and test

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,7 +5,8 @@ import (
 	"net/http"
 )
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+// HealthCheckHandler returns a health check
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	go w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"alive": true}`)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", healthCheckHandler)
+	mux.HandleFunc("/health-check", HealthCheckHandler)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
